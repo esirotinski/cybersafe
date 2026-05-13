41b3fd41b07dcc7491b042d606c0b1e949840fa9
Eugene Sirotinski <esirotinski@icloud.com>
2026-05-13 23:01:59 +0300

    Add files via upload

---
+++ 1778701927996.js
@@ -0,0 +1,8 @@
+// Get the current date and time
+const currentPath = new Date();
+
+// Print it to the console in a readable format
+console.log("Current Date and Time:", currentPath.toString());
+
+// Or, use a cleaner, localized format
+console.log("Localized Date:", currentPath.toLocaleString());
